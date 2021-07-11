8e85b918cf690accaa95f50741465e358abaf611
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-11 19:16:39 +0300

    add a profile model

--- myprofile/models.py
+++ myprofile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
+    photo = models.ImageField(upload_to = 'gallery/', null=True, blank=True, default='download.jpeg')
+    bio = models.CharField(max_length=300)
+    name = models.CharField(blank=True, max_length=120)
