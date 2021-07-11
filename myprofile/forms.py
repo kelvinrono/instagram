23a850c8af41c9e6a3f970171fffbb646b27f772
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-11 19:30:25 +0300

    create a form to update the profile

--- myprofile/forms.py
+++ myprofile/forms.py
@@ -4,3 +4,9 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('photo','name','bio')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'photo', 'bio']
