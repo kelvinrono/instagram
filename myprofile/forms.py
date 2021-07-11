3731510cf6e7cd74e2663aa8943c31eadd71e104
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-11 19:41:57 +0300

    add  image model

--- myprofile/forms.py
+++ myprofile/forms.py
@@ -1,12 +1,37 @@
 from django import forms
-from models import Profile
+from .models import Image,Profile,Comment
+from django.contrib.auth.models import User
+
+
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('image','caption')
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('photo','name','bio')
 
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
 
 class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['name', 'photo', 'bio']
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
--- myprofile/models.py
+++ myprofile/models.py
@@ -4,6 +4,45 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.exceptions import ObjectDoesNotExist
 
+
+# Create your models here.
+
+# likes = models.ManyToManyField(User, related_name='likes')
+class Image(models.Model):
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to = 'gallery/', null=True, blank=True)
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ["-pk"]
+
+   
+
+    @classmethod
+    def images(cls):
+        images = cls.objects.all()
+        return images
+
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls,old,new):
+        cap = Image.objects.filter(caption=old).update(caption=new)
+        return cap
+
+    def __str__(self):
+        return self.name
+
+    
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
     photo = models.ImageField(upload_to = 'gallery/', null=True, blank=True, default='download.jpeg')
@@ -41,7 +80,20 @@ class Profile(models.Model):
         return cls.objects.filter(user__username__icontains=name).all()
 
 
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
 
-    
+    def __str__(self):
+        return f'{self.follower} Follow'
 
+class Comment(models.Model):
+    comment = models.TextField()
+    user = models.ForeignKey('Profile',on_delete=models.CASCADE,related_name='comment')
+    photo = models.ForeignKey('Image',on_delete=models.CASCADE,related_name='comment')
 
+    class Meta:
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return f'{self.user.name} Image'
