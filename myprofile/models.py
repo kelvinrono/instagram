b856f189b2b947f4be8935df5be71b6ab18330ed
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-11 19:22:39 +0300

    add method to save and retrieve the profil

--- myprofile/models.py
+++ myprofile/models.py
@@ -1,8 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
     photo = models.ImageField(upload_to = 'gallery/', null=True, blank=True, default='download.jpeg')
     bio = models.CharField(max_length=300)
     name = models.CharField(blank=True, max_length=120)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    @classmethod
+    def profile(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+
+
+
+
+    
+
+
