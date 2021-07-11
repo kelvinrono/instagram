47a148151ae95a03b9f38f38e02fd2fc58660406
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-11 20:10:15 +0300

    create the urls for image and follow

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url,include
+from django.conf.urls import url
+from django.urls import path, include
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
-
 
 urlpatterns=[
     url('^$',views.home,name='home'),
@@ -10,9 +10,12 @@ urlpatterns=[
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url('profile/<str:username>/',views.profile,name='profile'),
     url('edit/profile/',views.update_profile,name='update'),
+    url('image/',views.post,name='post'),
+    url('search/', views.search_profile, name='search'),
     url('user_profile/<username>/', views.user_profile, name='user_profile'),
-
-
+    url('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    url('follow/<to_follow>', views.follow, name='follow'),
+    url('image/<id>', views.comment, name='comment'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- myprofile/views.py
+++ myprofile/views.py
@@ -8,11 +8,29 @@ from django.contrib.auth import login, authenticate
 from django.template.context_processors import csrf
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .email import send_welcome_email
+
 
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+@login_required(login_url='/accounts/login/')
+def index(request):
+    images = Image.images()
+    users = User.objects.exclude(id=request.user.id)
+    return render(request,'index.html', {"images":images[::1],"users":users})
+
+def post(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST,request.FILES)
+        print(form.errors)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return redirect('index')
+    else:
+        form = UploadForm()
+    return render(request,'post_image.html', {"form":form})
 
 @login_required(login_url='/accounts/login/')
 def profile(request, username):
@@ -34,3 +52,92 @@ def profile(request, username):
         'images': images,
     }
     return render(request, 'profile.html', params)
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        print(form.errors)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('profile')
+    else:
+        form = UploadForm()
+    return render(request,'edit_profile.html',{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def search_profile(request):
+    if 'search_user' in request.GET and request.GET['search_user']:
+        name = request.GET.get("search_user")
+        results = Profile.search_profile(name)
+        print(results)
+        message = f'name'
+        params = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'results.html', params)
+    else:
+        message = "You did not make a selection"
+    return render(request, 'results.html', {'message': message})
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_prof.profile.images.all()
+    
+    followers = Follow.objects.filter(followed=user_prof.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            follow_status = True
+        else:
+            follow_status = False
+    params = {
+        'user_prof': user_prof,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    return render(request, 'user_profile.html', params)
+
+@login_required(login_url='/accounts/login/')
+def unfollow(request, to_unfollow):
+    if request.method == 'GET':
+        user_two_profile = Profile.objects.get(pk=to_unfollow)
+        unfollow_d = Follow.objects.filter(follower=request.user.profile, followed=user_two_profile)
+        unfollow_d.delete()
+        return redirect('user_profile', user_two_profile.user.username)
+
+
+@login_required(login_url='/accounts/login/')
+def follow(request, to_follow):
+    if request.method == 'GET':
+        user_three_profile = Profile.objects.get(pk=to_follow)
+        follow_s = Follow(follower=request.user.profile, followed=user_three_profile)
+        follow_s.save()
+        return redirect('user_profile', user_three_profile.user.username)
+
+@login_required(login_url='/accounts/login/')
+def comment(request, id):
+    image = get_object_or_404(Image, pk=id)
+    comments = image.comment.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.photo = image
+            comment.user = request.user.profile
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = CommentForm()
+    params = {
+        'image': image,
+        'form': form,
+        'comments':comments,
+    }
+    return render(request, 'post.html', params)
